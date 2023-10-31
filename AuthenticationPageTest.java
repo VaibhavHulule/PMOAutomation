3462f99a44e8398a33513c73c25dc94eba3ee07e
VaibhavHulule <140307744+VaibhavHulule@users.noreply.github.com>
2023-10-31 15:42:38 +0530

    Add files via upload
    This project is created using PageObjectModel with help of selenium-testNg

---
+++ AuthenticationPageTest.java
@@ -0,0 +1,37 @@
+package tests.authenticationTest;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import pages.actionpages.authentication.LoginPageBase;
+import testdata.SiteListTestData;
+import tests.TestBase;
+
+
+public class AuthenticationPageTest extends TestBase {
+
+	LoginPageBase loginBase;
+
+
+	
+	SiteListTestData siteListTestData= new SiteListTestData();
+	@Override
+	@BeforeClass
+	protected void initPageObject() {
+		loginBase = new LoginPageBase(driverManager);
+	}
+
+	@Test(description = "Verify user is able to login using valid login credintials")
+	public void verify_valid_login() 
+	{
+
+		loginBase.login().fillUpCredintials(siteListTestData.accessCode, siteListTestData.username,
+				siteListTestData.password);
+		loginBase.siteSelection().selectSite(siteListTestData.siteName);
+		loginBase.selectModule().getAdministrator();
+		loginBase.selectModule().companyButton();
+		// loginBase.selectModule.companyButton();
+
+	}
+
+}
---
+++ SiteListTestData.java
@@ -0,0 +1,15 @@
+package testdata;
+
+public class SiteListTestData extends AuthTestData 
+{
+	
+	public String siteName ="XXXXX";
+	public String siteNameMaxixmo ="XXXXXX";
+	public String accessCode = "XXXXX";
+	public String username = "XXXXX.com";
+	public String password = "XXXX";
+	public String MaximoSite="XXXX";
+	public String ProductivitySite="XXXXX";
+
+			
+}
